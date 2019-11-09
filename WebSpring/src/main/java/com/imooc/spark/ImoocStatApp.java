67c244e15cbb2eda7ea88942c21de4742b5b0215
Raywcl <2728928100@qq.com>
2019-11-09 16:16:51 +0800

    1.Bug：合并两个项目后，Web项目前端访问：Uncaught ReferenceError：echarts is not defined at echants:23。【解决该问题：echarts.min.js和jquery.js指定位置的原因】

--- WebSpring/src/main/java/com/imooc/spark/ImoocStatApp.java
+++ WebSpring/src/main/java/com/imooc/spark/ImoocStatApp.java
@@ -1,12 +1,14 @@
 package com.imooc.spark;
 
 import com.imooc.dao.CourseClickCountDAO;
+import com.imooc.dao.CourseSearchClickCountDAO;
 import com.imooc.domain.CourseClickCount;
 //import com.sun.tools.internal.xjc.reader.RawTypeSet;
 //import com.sun.tools.internal.xjc.reader.xmlschema.bindinfo.BIConversion;
 
 //import net.sf.json;
 //import net.sf.json.JSONArray;
+import com.imooc.domain.CourseSearchClickCount;
 import net.sf.json.JSONArray;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -40,7 +42,7 @@ public class ImoocStatApp {
     @Autowired
     CourseClickCountDAO courseClickCountDAO;
     @Autowired
-    CourseClickCountDAO courseSearchClickCountDAO;
+    CourseSearchClickCountDAO courseSearchClickCountDAO;
 //    @RequestMapping(value="/course_clickcount_dynamic",method = RequestMethod.GET)
 //   public ModelAndView courseClickCount() throws Exception{
 //        ModelAndView view=new ModelAndView("index");
@@ -56,6 +58,14 @@ public class ImoocStatApp {
 //        return  view;
 //    }
 
+    /**
+     * 获得某日某各个课程的点击量
+     * @return
+     * @throws Exception
+     * 验证：
+     *
+     *
+     */
     @RequestMapping(value="/course_clickcount_dynamic",method = RequestMethod.POST)
     @ResponseBody
     public List<CourseClickCount> courseClickCount() throws Exception{
@@ -80,6 +90,51 @@ public class ImoocStatApp {
 //        return view;
     }
 
+    /**
+     * 获得某日某个引擎引流过来的点击量
+     * @return
+     * @throws Exception
+     * 验证：
+     [
+    {
+    "name": "Spark SQL慕课网日志分析",
+    "value": 2
+    },
+    {
+    "name": "深度学习之神经网络核心原理与算法",
+    "value": 2
+    }
+    ]
+     */
+    @RequestMapping(value="/course_searchclickcount_dynamic",method = RequestMethod.POST)
+    @ResponseBody
+    public List<CourseSearchClickCount> courseSearchClickCount() throws Exception{
+//    public ModelAndView courseClickCount() throws Exception{
+        //自己添加修改20181126，否则总是一天的。
+//        SimpleDateFormat df = new SimpleDateFormat("yyyyMMdd");
+//        String date = df.format(new Date());
+
+        List<CourseSearchClickCount> list = courseSearchClickCountDAO.queryDayEngineCourse("20191109","search.yahoo.com");
+        //        List<CourseClickCount> list = courseClickCountDAO.query(date);
+        for (CourseSearchClickCount model:list ){
+            try {
+                String name=model.getName().split("_")[2];
+                model.setName(courses.get(name));
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+        return  list;
+
+//        ModelAndView view=new ModelAndView("index");
+//        List<CourseClickCount> list = courseClickCountDAO.query("20191107");
+//        for (CourseClickCount model:list ){
+//            model.setName(courses.get(model.getName().substring(9)));
+//        }
+//        JSONArray json=JSONArray.fromObject(list);
+//        view.addObject("data_json",json);
+//        return view;
+    }
 
     @RequestMapping(value = "/echarts",method = RequestMethod.GET)
     public ModelAndView echarts(){
